33000e84dbf18526cc33f5b43ae54ed0573bf0fa
Goncalo Miguel Martins Coelho <newmaximus0@gmail.com>
2018-12-21 18:19:53 +0000

    - Projeto com programação Asyncrona em Winforms

--- Cap11_MultiTask_02_Threads_WinForm/Form1.Designer.cs
+++ Cap11_MultiTask_02_Threads_WinForm/Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonDoAsyncStuff = new System.Windows.Forms.Button();
+            this.buttonDoSyncStuff = new System.Windows.Forms.Button();
+            this.buttonDoAsyncStuffRight = new System.Windows.Forms.Button();
+            this.pictureBoxGif = new System.Windows.Forms.PictureBox();
+            this.groupBoxOutput = new System.Windows.Forms.GroupBox();
+            this.labelContador = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGif)).BeginInit();
+            this.groupBoxOutput.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonDoAsyncStuff
+            // 
+            this.buttonDoAsyncStuff.Location = new System.Drawing.Point(108, 172);
+            this.buttonDoAsyncStuff.Name = "buttonDoAsyncStuff";
+            this.buttonDoAsyncStuff.Size = new System.Drawing.Size(204, 116);
+            this.buttonDoAsyncStuff.TabIndex = 2;
+            this.buttonDoAsyncStuff.Text = "Do Async Stuff";
+            this.buttonDoAsyncStuff.UseVisualStyleBackColor = true;
+            this.buttonDoAsyncStuff.Click += new System.EventHandler(this.buttonDoAsyncStuff_Click);
+            // 
+            // buttonDoSyncStuff
+            // 
+            this.buttonDoSyncStuff.Location = new System.Drawing.Point(108, 50);
+            this.buttonDoSyncStuff.Name = "buttonDoSyncStuff";
+            this.buttonDoSyncStuff.Size = new System.Drawing.Size(204, 116);
+            this.buttonDoSyncStuff.TabIndex = 3;
+            this.buttonDoSyncStuff.Text = "Do SyncStuff";
+            this.buttonDoSyncStuff.UseVisualStyleBackColor = true;
+            this.buttonDoSyncStuff.Click += new System.EventHandler(this.buttonDoSyncStuff_Click);
+            // 
+            // buttonDoAsyncStuffRight
+            // 
+            this.buttonDoAsyncStuffRight.Location = new System.Drawing.Point(108, 294);
+            this.buttonDoAsyncStuffRight.Name = "buttonDoAsyncStuffRight";
+            this.buttonDoAsyncStuffRight.Size = new System.Drawing.Size(204, 116);
+            this.buttonDoAsyncStuffRight.TabIndex = 4;
+            this.buttonDoAsyncStuffRight.Text = "Do Async Stuff Right";
+            this.buttonDoAsyncStuffRight.UseVisualStyleBackColor = true;
+            this.buttonDoAsyncStuffRight.Click += new System.EventHandler(this.buttonDoAsyncStuffRight_Click);
+            // 
+            // pictureBoxGif
+            // 
+            this.pictureBoxGif.Image = global::Cap11_MultiTask_02_Threads_WinForm.Properties.Resources.Gif;
+            this.pictureBoxGif.Location = new System.Drawing.Point(460, 50);
+            this.pictureBoxGif.Name = "pictureBoxGif";
+            this.pictureBoxGif.Size = new System.Drawing.Size(294, 210);
+            this.pictureBoxGif.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxGif.TabIndex = 5;
+            this.pictureBoxGif.TabStop = false;
+            // 
+            // groupBoxOutput
+            // 
+            this.groupBoxOutput.Controls.Add(this.labelContador);
+            this.groupBoxOutput.Location = new System.Drawing.Point(535, 310);
+            this.groupBoxOutput.Name = "groupBoxOutput";
+            this.groupBoxOutput.Size = new System.Drawing.Size(190, 100);
+            this.groupBoxOutput.TabIndex = 6;
+            this.groupBoxOutput.TabStop = false;
+            this.groupBoxOutput.Text = "OutPut";
+            // 
+            // labelContador
+            // 
+            this.labelContador.AutoSize = true;
+            this.labelContador.Font = new System.Drawing.Font("Microsoft Sans Serif", 39.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelContador.Location = new System.Drawing.Point(17, 26);
+            this.labelContador.Name = "labelContador";
+            this.labelContador.Size = new System.Drawing.Size(56, 61);
+            this.labelContador.TabIndex = 0;
+            this.labelContador.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBoxOutput);
+            this.Controls.Add(this.pictureBoxGif);
+            this.Controls.Add(this.buttonDoAsyncStuffRight);
+            this.Controls.Add(this.buttonDoSyncStuff);
+            this.Controls.Add(this.buttonDoAsyncStuff);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGif)).EndInit();
+            this.groupBoxOutput.ResumeLayout(false);
+            this.groupBoxOutput.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonDoAsyncStuff;
+        private System.Windows.Forms.Button buttonDoSyncStuff;
+        private System.Windows.Forms.Button buttonDoAsyncStuffRight;
+        private System.Windows.Forms.PictureBox pictureBoxGif;
+        private System.Windows.Forms.GroupBox groupBoxOutput;
+        private System.Windows.Forms.Label labelContador;
     }
 }
 
--- Cap11_MultiTask_02_Threads_WinForm/Form1.cs
+++ Cap11_MultiTask_02_Threads_WinForm/Form1.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +9,68 @@ namespace Cap11_MultiTask_02_Threads_WinForm
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Método de ação ao evento do botão. Síncrono
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonDoSyncStuff_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 50000; i++)    
+            {
+                labelContador.Text = i.ToString();  
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void buttonDoAsyncStuff_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>    // Início da nova Thread
+            { 
+                for (int i = 0; i <= 50000; i++)
+                {
+                    labelContador.Text = i.ToString();  // Exceção aceder ao objeto da thread principal
+                }
+            }); // Fim da nova Thread
+        }
+
+        /// <summary>
+        /// Método de ação ao evento do botão. Assíncrono
+        /// Neste caso é criada uma nova thread a cada iteração do ciclo que devolve para a thread principal uma string
+        /// com o valor.
+        /// Já na thread principal o valor é colocado na Label
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void buttonDoAsyncStuffRight_Click(object sender, EventArgs e)
+        {
+            // Desativa os botões para não interferir com a contagem, porque bloqueia  
+            buttonDoSyncStuff.Enabled = false;
+            buttonDoAsyncStuff.Enabled = false;
+            buttonDoAsyncStuffRight.Enabled = false;
+
+            pictureBoxGif.Visible = true;
+            pictureBoxGif.Image = Properties.Resources.Gif;
+
+            for (int i = 0; i <= 50000; i++)
+            {
+                labelContador.Text = await Task.Run(() =>    // Início da nova Thread
+                {
+                    Task.Delay(200);            // Pausa na execução da Thread paralela
+                    return i.ToString();        // Devolve o valor para o Thread Principal
+                }); // Fim da nova Thread
+            }
+
+            // Reativa os botões após a conclusão da Thread
+            buttonDoSyncStuff.Enabled = true;
+            buttonDoAsyncStuff.Enabled = true;
+            buttonDoAsyncStuffRight.Enabled = true;
         }
     }
 }
